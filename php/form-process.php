d47a1be5c631d573478f52be80e37276e4bdd36e
geotan <tanuk5@hotmail.com>
2019-12-31 18:24:56 +0100

    php mail sending - not tested

--- php/form-process.php
+++ php/form-process.php
@@ -4,21 +4,21 @@ $errorMSG = "";
 
 // NAME
 if (empty($_POST["name"])) {
-    $errorMSG = "Name is required ";
+    $errorMSG = "Manca il nome";
 } else {
     $name = $_POST["name"];
 }
 
 // EMAIL
 if (empty($_POST["email"])) {
-    $errorMSG .= "Email is required ";
+    $errorMSG .= "Manca l'email";
 } else {
     $email = $_POST["email"];
 }
 
 // MSG SUBJECT
 if (empty($_POST["msg_subject"])) {
-    $errorMSG .= "Subject is required ";
+    $errorMSG .= "Inserire un oggetto";
 } else {
     $msg_subject = $_POST["msg_subject"];
 }
@@ -26,32 +26,32 @@ if (empty($_POST["msg_subject"])) {
 
 // MESSAGE
 if (empty($_POST["message"])) {
-    $errorMSG .= "Message is required ";
+    $errorMSG .= "Manca il messaggio";
 } else {
     $message = $_POST["message"];
 }
 
 
 $EmailTo = "email@gmail.com";
-$Subject = "New Message Received";
+$Subject = "AlanPromo - Richiesta di contatto dal sito";
 
 // prepare email body text
 $Body = "";
-$Body .= "Name: ";
+$Body .= "Nome: ";
 $Body .= $name;
 $Body .= "\n";
 $Body .= "Email: ";
 $Body .= $email;
 $Body .= "\n";
-$Body .= "Subject: ";
+$Body .= "Oggetto: ";
 $Body .= $msg_subject;
 $Body .= "\n";
-$Body .= "Message: ";
+$Body .= "Messaggio: ";
 $Body .= $message;
 $Body .= "\n";
 
 // send email
-$success = mail($EmailTo, $Subject, $Body, "From:".$email);
+$success = mail($EmailTo, $Subject, $Body, "Da:".$email);
 
 // redirect to success page
 if ($success && $errorMSG == ""){
